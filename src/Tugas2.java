9d2265d5357775dffe99e630c02c0844054dc9f5
E.N.D <E.N.D>
2016-03-09 19:50:33 +0700

    Awal

--- src/Tugas2.java
+++ src/Tugas2.java
@@ -1,9 +1,35 @@
-package sc;
 import java.util.Scanner;
 public class Tugas2{
-	private int keliling, jarak;
-	private int jumlahPutaran, jumlah_waktu;
-	private String nama_mahasiswa, nim;
+	private int jumlah, panjang, lebar, tmp=0, a=0;
 
+	private int[] keliling;
+	private int[] jarak;
+	private int[] jumlahPutaran;
+	private int[] jumlah_waktu;
+	private String[] nama_mahasiswa;
+	private String[] nim;
 
+public void jumlah(int j){
+	jumlah = j;
+	keliling  = new int [jumlah];
+	jarak  = new int [jumlah];
+	jumlahPutaran  = new int [jumlah];
+	jumlah_waktu = new int [jumlah];
+	nama_mahasiswa = new String [jumlah];
+	nim = new String [jumlah];
+
+}
+public void lpg(int p, int l){
+	panjang = p;
+	lebar = l;
+}
+public void Input(){
+	Scanner in = new Scanner (System.in);
+	for (int i=0;i<jumlah;i++) {
+		nim[i] = in.next();
+		nama_mahasiswa[i] = in.next();
+		jumlahPutaran[i] = in.nextInt();
+		jumlah_waktu[i] = in.nextInt();
+	}
+}
 }
